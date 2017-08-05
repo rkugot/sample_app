bc5f09ff60aab2e3f86ee6a7b18a8c32d5bb9d73
rkugot <stream13k@gmail.com>
2017-08-05 11:58:28 +0300

    Add Contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,4 +16,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
 end
