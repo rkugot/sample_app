7ca27462e4ee49e20ff48c02d66626ecb730d51c
rkugot <stream13k@gmail.com>
2017-08-16 14:28:33 +0300

    Add admin column to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "qwerty",
-             password_confirmation: "qwerty")
+             password_confirmation: "qwerty",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
